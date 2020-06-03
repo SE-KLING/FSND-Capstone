b1cb00664e537b95e351675a5b772bfb3c792a24
Stefan Klingenberg <stefan@byteorbit.com>
2020-06-03 13:52:34 +0200

    Refreshed JWT tokens and updated requirements.txt

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-export AUTH0_DOMAIN='nanodacity.auth0.com'
-export ALGORITHMS=['RS256']
-export API_AUDIENCE='casting'
-export SQLALCHEMY_DATABASE_URI='postgres://localhost:5432/casting_agency'
+#export AUTH0_DOMAIN='nanodacity.auth0.com'
+#export ALGORITHMS=['RS256']
+#export API_AUDIENCE='casting'
+#export SQLALCHEMY_DATABASE_URI='postgres://localhost:5432/casting_agency'
+
+https://nanodacity.auth0.com/authorize?audience=casting&response_type=token&client_id=R4Uj87agYYjttwpqf1o9qyB1YjsgLgxp&redirect_uri=http://127.0.0.1:8080
